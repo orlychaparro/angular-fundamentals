ab57897453a430e70c261df672031f3e69f2ce51
orlychaparro <orlychaparro@gmail.com>
2023-06-04 15:45:09 -0400

    Ejercicios Property binging, data binging con ngModel AgularForms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,37 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name = 'Nicolas';
   age = 18;
-  img = "https://startupxplore.com/uploads/ff8080815235115101523595712a00de-large.png";
+ // img = "https://startupxplore.com/uploads/ff8080815235115101523595712a00de-large.png";
+ img = "https://www.w3schools.com/howto/img_avatar.png";
+ btnDisabled = true;
+
+ person = {
+  name: 'Nicolas',
+  age:18,
+  avatar:'https://www.w3schools.com/howto/img_avatar.png'
+ }
+
+ toggleButton(){
+  this.btnDisabled = !this.btnDisabled;
+ }
+
+ increaseAge(){
+  this.person.age += 1;
+
+ }
+
+ onScroll(event: Event){
+  const element =event.target as HTMLElement;
+  console.log(element.scrollTop);
+
+
+}
+
+changeName(event: Event){
+
+  const element = event.target as HTMLInputElement;
+  this.person.name = element.value
+
+}
 
 }
